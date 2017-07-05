90704109f9460a947116a2bd0833863b01ddf0d0
Brian OConnell <boc@us.ibm.com>
2017-07-05 08:41:34 -0400

    Code path cleanup.

--- lib/kitchen_blame.rb
+++ lib/kitchen_blame.rb
@@ -18,6 +18,7 @@ require 'date'
 require 'json'
 
 module KitchenBlame
+  # Main class
   class Blame
     # Return the DateTime object from a regex match with a named capture of 'time'
     def self.extract_time(match)
@@ -44,47 +45,62 @@ module KitchenBlame
     end
 
     def self.analyze_create(key_path, log)
-      create_line = File.foreach(log).grep(/Creating/).first
-      match_data = /.,\s\[(?<time>.*)\]\s+[^:]+:(?<log>.*)$/.match(create_line)
-      create_time = extract_time(match_data)
-      ip = File.foreach(log).grep(/Attaching floating IP </).first[/\<(?<ip>.*)\>/, 'ip']
-      server_log_entries = `ssh -i #{key_path} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vagrant@#{ip} 'sudo journalctl -o json --no-pager' 2>/dev/null`.split("\n")
-      server_log_entries = server_log_entries.map { |entry| JSON.parse(entry) }
-      boot_start = extract_syslog_time(server_log_entries.first)
-      create_duration = measure_duration(create_time, boot_start)
-
+      server_log_entries = server_log_entries(key_path, log)
+      create_duration = image_create(log, server_log_entries)
       startup_finished_entry = server_log_entries.find { |entry| entry['MESSAGE'].include?('Startup finished') }
+      boot_start = extract_syslog_time(server_log_entries.first)
       boot_finish = extract_syslog_time(startup_finished_entry)
       boot_duration = measure_duration(boot_start, boot_finish)
-      puts "Create took #{create_duration} seconds"
+      puts "Image create took #{create_duration} seconds"
       puts "Boot took #{boot_duration} seconds"
       puts "Systemd timing: #{startup_finished_entry['MESSAGE']}"
     end
 
+    def self.server_log_entries(key_path, log)
+      ip = File.foreach(log).grep(/Attaching floating IP </).first[/\<(?<ip>.*)\>/, 'ip']
+      server_log_entries = `ssh -i #{key_path} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vagrant@#{ip} 'sudo journalctl -o json --no-pager' 2>/dev/null`.split("\n")
+      server_log_entries.map { |entry| JSON.parse(entry) }
+    end
+
+    def self.image_create(log, server_log_entries)
+      boot_start = extract_syslog_time(server_log_entries.first)
+      create_line = File.foreach(log).grep(/Creating/).first
+      match_data = /.,\s\[(?<time>.*)\]\s+[^:]+:(?<log>.*)$/.match(create_line)
+      create_time = extract_time(match_data)
+      measure_duration(create_time, boot_start)
+    end
+
+    # rubocop:disable MethodLength
     def self.analyze_recipe(log)
-      IO.readlines(log).grep(/Recipe:/).map do |line|
+      recipe_lines = IO.readlines(log).grep(/Recipe:/).map do |line|
         match_data = /\[(?<time>.*)\].*Recipe:\s(?<recipe>.*)$/.match(line)
         time = extract_time(match_data)
         recipe = match_data[:recipe]
         { recipe: recipe, time: time }
-      end.each_cons(2) do |pair|
+      end
+      recipe_lines.each_cons(2) do |pair|
         recipe = pair.first[:recipe]
         duration = measure_pair(pair)
         puts "#{duration} seconds for recipe #{recipe}"
       end
     end
+    # rubocop:enable MethodLength
 
+    # rubocop:disable MethodLength
     def self.analyze_duration(log)
-      IO.readlines(log).map do |line|
+      log_lines = IO.readlines(log).map do |line|
         match_data = /.,\s\[(?<time>.*)\]\s+[^:]+:(?<log>.*)$/.match(line)
+        puts match_data
         time = extract_time(match_data)
         log = match_data[:log]
         { log: log, time: time }
-      end.each_cons(2) do |pair|
+      end
+      log_lines.each_cons(2) do |pair|
         log = pair.first[:log]
         duration = measure_pair(pair)
         puts "#{duration} seconds for entry #{log}"
       end
     end
+    # rubocop:enable MethodLength
   end
 end
